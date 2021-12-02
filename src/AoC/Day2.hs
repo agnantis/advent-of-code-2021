a8eba57bf3a3cb3403eec2609f2aae5a509ce5a3
Kostas Agnantis <kagnadis@gmail.com>
2021-12-02 23:03:33 +0200

    Day 2 - Part 2 (+ Part 1 refactoring)

--- src/AoC/Day2.hs
+++ src/AoC/Day2.hs
@@ -32,35 +32,70 @@ Calculate the horizontal position and depth you would have after following the p
 Your puzzle answer was 1762050.
 
 The first half of this puzzle is complete! It provides one gold star: *
+
+--- Part Two ---
+Based on your calculations, the planned course doesn't seem to make any sense. You find the submarine manual and discover that the process is actually slightly more complicated.
+
+In addition to horizontal position and depth, you'll also need to track a third value, aim, which also starts at 0. The commands also mean something entirely different than you first thought:
+
+down X increases your aim by X units.
+up X decreases your aim by X units.
+forward X does two things:
+It increases your horizontal position by X units.
+It increases your depth by your aim multiplied by X.
+Again note that since you're on a submarine, down and up do the opposite of what you might expect: "down" means aiming in the positive direction.
+
+Now, the above example does something different:
+
+forward 5 adds 5 to your horizontal position, a total of 5. Because your aim is 0, your depth does not change.
+down 5 adds 5 to your aim, resulting in a value of 5.
+forward 8 adds 8 to your horizontal position, a total of 13. Because your aim is 5, your depth increases by 8*5=40.
+up 3 decreases your aim by 3, resulting in a value of 2.
+down 8 adds 8 to your aim, resulting in a value of 10.
+forward 2 adds 2 to your horizontal position, a total of 15. Because your aim is 10, your depth increases by 2*10=20 to a total of 60.
+After following these new instructions, you would have a horizontal position of 15 and a depth of 60. (Multiplying these produces 900.)
+
+Using this new interpretation of the commands, calculate the horizontal position and depth you would have after following the planned course. What do you get if you multiply your final horizontal position by your final depth?
+
+Your puzzle answer was 1855892637.
+
+Both parts of this puzzle are complete! They provide two gold stars: **
 -}
 
 module AoC.Day2 where
 
 import Control.Arrow ((&&&))
+import Data.List (foldl')
 
 type Input = [(String, Int)]
+
 type Output = Int
 
--- | 
+-- |
 -- >>> readCommand "forward 12"
 -- ("forward", 12)
 readCommand :: String -> (String, Int)
-readCommand cmd = 
+readCommand cmd =
   let [x, v] = words cmd
-  in (x, read v)
+   in (x, read v)
 
 fstStar :: Input -> Output
-fstStar = uncurry (*) . foldr f (0,0)
- where f ("forward", x) (a, b) = (a+x, b)
-       f ("up", x) (a, b) = (a, b-x)
-       f ("down", x) (a, b) = (a, b+x)
-       f _ (a, b) = (a, b)
+fstStar = uncurry (*) . foldl' f (0, 0)
+  where
+    f (h, d) ("forward", x) = (h + x, d)
+    f (h, d) ("up", x) = (h, d - x)
+    f (h, d) ("down", x) = (h, d + x)
+    f t _ = t
 
 sndStar :: Input -> Output
-sndStar xs = undefined
+sndStar = (\(h, d, _) -> h * d) . foldl' f (0, 0, 0)
+  where
+    f (h, d, a) ("forward", x) = (h + x, d + (x * a), a)
+    f (h, d, a) ("up", x) = (h, d, a - x)
+    f (h, d, a) ("down", x) = (h, d, a + x)
+    f t _ = t
 
-main :: IO  ()
+main :: IO ()
 main = do
   input <- fmap readCommand . lines <$> readFile "src/input/day2"
   print . (fstStar &&& sndStar) $ input
-
